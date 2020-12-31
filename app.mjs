b666f4d5fecd2f4b1e0be862d57dafb7717c2686
catpea <corporate.miou@gmail.com>
2020-12-30 21:22:38 -0500

    New Release

--- app.mjs
+++ app.mjs
@@ -4,7 +4,6 @@ import fs from 'fs';
 import path from 'path';
 
 import lodash from 'lodash';
-import truncate from 'lodash/truncate.js';
 import pct from 'calculate-percent';
 
 
@@ -16,48 +15,38 @@ import serve from 'koa-static';
 import logger from 'koa-logger';
 
 import render from './lib/render.mjs';
+import datasource from './lib/datasource.mjs';
 
 async function main(){
 
 const app = new Koa();
+
+app.use(async (ctx,next) => {
+
+
+  ctx.state.title = 'Cat Pea University';
+  ctx.state.description = 'We offer the latest in teaching, learning, and research. Our aim is to inspire students to knowledge, wisdom, and greatness for the betterment of Human Kind.';
+
+
+  await next();
+});
+
 app.use(serve('static'));
 
 
 const router = koaRouter();
 
+
+
 // "database"
 const options = {
+  debug: true,
   sourceDatabasePath: 'posts',
   extension: '.json',
+  limit: 10,
 }
 
-const posts = fs.readdirSync(path.resolve(options.sourceDatabasePath), { withFileTypes: true })
-.filter(fileObject => fileObject.isFile())
-.map(fileObject => fileObject.name)
-.filter(name => name.endsWith(options.extension))
-.sort()
-.map(name => fs.readFileSync( path.join(path.resolve(options.sourceDatabasePath), name) ).toString() )
-.map( string => JSON.parse(string).reverse() )
-.flat()
-.map(data=>{
-  data.html = data.data.html;
-  data.text = data.data.text;
-  data.text = truncate(data.text, {
-  'length': 512,
-  'separator': /,? +/
-  });
-  return data;
-})
-
-posts.forEach((o,i)=>{
-  o.counter = (posts.length-i);
-})
-
-posts.forEach((o,i)=>{
-  o.forward = (i-1<0?posts.length:i-1);
-  o.backward = (i+1>posts.length?0:i+1);
-})
-
+const data = datasource(options);
 
 
 
@@ -73,14 +62,23 @@ app.use(koaBody());
 
 router
   .get('/', index)
+
+  .get('/book/:name/:order/read/:counter', book)
+  .get('/book/:name/:order/page/:page', book)
+  .get('/book/:name/:order', book)
+
+
+
+
+
   .get('/browse/:page', index)
-  .get('/read/:counter', read)
+  .get('/read/:name/:counter', read)
 
   .get('/list', list)
 
-  .get('/post/:id', show)
+  //.get('/post/:id', show)
 
-  .post('/post', create);
+  //.post('/post', create);
 
 app.use(router.routes());
 
@@ -90,31 +88,46 @@ app.use(router.routes());
 
 
 async function index(ctx) {
-  // [].concat(posts).reverse()
-  const limit = 10;
-  const pages = Math.floor((posts.length)/limit);
-  const page = ctx.params.page?(parseInt(ctx.params.page)-1):pages;
 
-  const prev = page-1;
-  const next = page+1;
+  const {pages, posts} = data.all.latest;
 
-
-  const sliced = lodash([].concat(posts).reverse()).drop(page * limit).take(limit).value(); //.orderBy(['title'], ['asc'])
-  const flipped = [].concat(sliced).reverse();
+  const pageNumber = ctx.params.page?(parseInt(ctx.params.page)):(pages.length-1);
+  const selected = lodash.filter(posts, { pageCounter:pageNumber });
 
   await ctx.render('index', {
-    pagination: {
 
-      prev: (prev<0?pages:prev)+1,
-      next: (next>pages?0:next)+1,
+    pageName:        ctx.state.title,
+    pageDescription: ctx.state.description,
 
-      first: page==1?true:false,
-      last: page==pages?true:false,
-      second: page==(pages-2)?true:false,
-    },
-    // [].concat(posts).reverse()
-    posts: flipped
+    pagination: lodash.last(selected),
+    books: data.meta.books,
+    posts: selected
   });
+
+}
+
+
+async function book(ctx) {
+
+  const meta = data.meta.books.filter(o=>o.name === ctx.params.name).pop();
+  const order = ctx.params.order?ctx.params.order:meta.order;
+  const {pages, posts} = data[ctx.params.name][order];
+  const pageNumber = ctx.params.page?(parseInt(ctx.params.page)):(order=='story'?0:(pages.length-1))
+  const selected = lodash.filter(posts, { pageCounter:pageNumber });
+
+  await ctx.render('book', {
+
+    bookName:        meta.name,
+    pageName:        `${meta.title}: ${meta.subtitle}`,
+    pageDescription: `${meta.description}`,
+    currentSort:      order,
+    defaultSort:      meta.order, // default
+
+    pagination: lodash.last(selected),
+    books: data.meta.books,
+    posts: selected
+  });
+
 }
 
 
@@ -127,24 +140,19 @@ async function index(ctx) {
 
 
 async function read(ctx) {
-  const size = posts.length;
-  const index = parseInt(ctx.params.counter)-1;
-  const post = posts[index];
+
+  const meta = data.meta.books.filter(o=>o.name === ctx.params.name).pop();
+  const order = ctx.params.order?ctx.params.order:meta.order;
+  const {pages, posts} = data[ctx.params.name][order];
+  const post = lodash.find(posts, function(o) { return o.counter == parseInt(ctx.params.counter) });
+
   if (!post) ctx.throw(404, 'invalid post id');
-  const previousNumber = index-1;
-  const nextNumber = index+1;
 
   await ctx.render('read', {
-    pagination: {
-      index,
-      size,
-      prev: ((previousNumber<0)?size-1:previousNumber)+1,
-      next: ((nextNumber>size-1)?0:nextNumber)+1,
-      first: previousNumber<0?true:false,
-      last: nextNumber>size-1?true:false,
-      percent: pct(index+1, size),
+    pageName: post.title,
+    pageDescription: `${meta.title}: ${meta.description}`,
 
-    },
+    books: data.meta.books,
     post,
   });
 }
@@ -158,42 +166,42 @@ async function read(ctx) {
 
 async function list(ctx) {
   await ctx.render('list', {
-    posts,
+    posts: data.all.posts,
   });
 }
-
-
-
-/**
- * Show creation form.
- */
-
-async function add(ctx) {
-  await ctx.render('new');
-}
-
-/**
- * Show post :id.
- */
-
-async function show(ctx) {
-  const id = ctx.params.id;
-  const post = posts[id];
-  if (!post) ctx.throw(404, 'invalid post id');
-  await ctx.render('show', { post });
-}
-
-/**
- * Create a post.
- */
-
-async function create(ctx) {
-  const post = ctx.request.body;
-  const id = posts.push(post) - 1;
-  post.created_at = new Date();
-  post.id = id;
-  ctx.redirect('/');
-}
+//
+//
+//
+// /**
+//  * Show creation form.
+//  */
+//
+// async function add(ctx) {
+//   await ctx.render('new');
+// }
+//
+// /**
+//  * Show post :id.
+//  */
+//
+// async function show(ctx) {
+//   const id = ctx.params.id;
+//   const post = posts[id];
+//   if (!post) ctx.throw(404, 'invalid post id');
+//   await ctx.render('show', { post });
+// }
+//
+// /**
+//  * Create a post.
+//  */
+//
+// async function create(ctx) {
+//   const post = ctx.request.body;
+//   const id = posts.push(post) - 1;
+//   post.created_at = new Date();
+//   post.id = id;
+//   ctx.redirect('/');
+// }
 
 // listen
 app.listen(3000);
