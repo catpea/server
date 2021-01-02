079bfe1aa0d6dc5df932fc9542b4daa6aeda0eb2
catpea <corporate.miou@gmail.com>
2021-01-02 18:16:38 -0500

    Added a publish script for consistency with all other components.

---
+++ bin/publish/index.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+git add .;
+
+git commit -m "New website generation $(date)";
+
+git push;
---
+++ publish.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# freindly and consitent alias for internal command.
+
+./bin/publish/index.sh
