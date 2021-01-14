9d3b981e32f08453443af5ed7e446f217150f761
catpea <corporate.miou@gmail.com>
2021-01-13 21:23:58 -0500

    New website generation Wed Jan 13 09:23:58 PM EST 2021

--- bin/static/index.sh
+++ bin/static/index.sh
@@ -5,6 +5,6 @@ echo Creating Static Version
 rm -fr ./docs/
 mkdir -p docs
 
-echo WGET MIRRORING (this may take a while)
+echo WGET MIRRORING "(this may take a while)"
 wget --quiet --mirror --convert-links --html-extension --no-host-directories --directory-prefix ./docs/ http://black:7467
 cp ./views/404.html ./docs/404.html
