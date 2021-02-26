1193d46106c7ccadb64f3c704085db46e0bb3216
catpea <corporate.miou@gmail.com>
2021-02-26 14:24:40 -0500

    New website generation Fri Feb 26 02:24:40 PM EST 2021

--- bin/static/index.sh
+++ bin/static/index.sh
@@ -8,3 +8,5 @@ mkdir -p docs
 echo WGET MIRRORING "(this may take a while)"
 wget --quiet --mirror --convert-links --html-extension --no-host-directories --directory-prefix ./docs/ http://black:7467
 cp ./views/404.html ./docs/404.html
+ 
+rsync -qav --progress ./static/audio/ ./docs/audio
